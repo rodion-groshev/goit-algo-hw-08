807f038c281440882d077bda2a8bd1c31d092cf8
Rodion <rodion.groshev@gmail.com>
2024-03-12 08:21:30 +0100

    hw_8 rework

--- hw_8_task_1.py
+++ hw_8_task_1.py
@@ -2,27 +2,24 @@ import heapq
 
 
 def heap_sort_desc(iterable):
-    if len(iterable) % 2 != 0:
-        raise Exception("Not enough cables to unite. Add one more.")
     h = []
     heapq.heapify(iterable)
+    total = 0
 
     for value in iterable:
         heapq.heappush(h, value)
-    min_values = [heapq.heappop(h) for _ in range(len(h))]
-    min_values = min_values[:len(min_values) // 2]
-    print(f"Min values: {min_values}")
 
-    for value in iterable:
-        heapq.heappush(h, -value)
-    max_values = [-heapq.heappop(h) for _ in range(len(h))]
-    max_values = max_values[:len(max_values) // 2]
-    print(f"Min values: {max_values}")
+    while len(h) > 1:
+        cable_1 = heapq.heappop(h)
+        cable_2 = heapq.heappop(h)
 
-    res = [a + b for a, b in zip(max_values, min_values)]
-    print(f"Sum of two: {res}")
-    return sum(res)
+        sum_of_2 = cable_1 + cable_2
+        total += sum_of_2
+
+        heapq.heappush(h, sum_of_2)
+
+    return total
 
 
-arr = [12, 11, 13, 5, 6, 7]
+arr = [4, 6, 8, 12]
 print("Sum of all:", heap_sort_desc(arr))
